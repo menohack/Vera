d22cd1ef2366718203d9d67a975d7be5cb7a74d1
James Doverspike <menohack@gmail.com>
2014-02-06 21:15:10 -0500

    Added cube spawning and the build script

---
+++ Assets/Build.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Build : MonoBehaviour {
+
+	public Transform buildObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.E))
+		{
+			GameObject go = Instantiate(buildObject, transform.position + transform.forward * 5.0f, transform.rotation) as GameObject;
+		}
+	}
+}
