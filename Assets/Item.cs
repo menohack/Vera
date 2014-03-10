e321c9340e43f8da55205f790f8111ef3a37acbf
James Doverspike <menohack@gmail.com>
2014-03-10 18:43:14 -0400

    Added minimum distance between buildings

--- Assets/Item.cs
+++ Assets/Item.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class Item : MonoBehaviour {
 
+	/// <summary>
+	/// The starting material of the item.
+	/// </summary>
 	Material originalMaterial;
+
+	/// <summary>
+	/// Counts the number of intersections with the environment. Zero means no intersection.
+	/// </summary>
 	int overlapCount = 0;
+
+	/// <summary>
+	/// Whether this item has been placed yet.
+	/// </summary>
 	bool placed = false;
 
+	/// <summary>
+	/// The layer of the environment.
+	/// </summary>
 	int layerMask;
 
+	/// <summary>
+	/// The minimum distance two buildings that are not connected may be.
+	/// </summary>
+	public float minBuildingDistance = 10.0f;
 	
+	/// <summary>
+	/// Initialization.
+	/// </summary>
 	void Start () {
 		originalMaterial = renderer.material;
 		layerMask = LayerMask.NameToLayer("Environment");
@@ -23,19 +44,21 @@ public class Item : MonoBehaviour {
 		if (!placed)
 		{
 			renderer.material = new Material(originalMaterial);
-			if (overlapCount == 0)
-				renderer.material.color = new Color(1.0f, 1.0f, 1.0f, 0.6f) * Color.red;
+			renderer.material.color = new Color(1.0f, 1.0f, 1.0f, 0.6f);
+			if (CanPlace())
+				renderer.material.color *= Color.green;
 			else
-				renderer.material.color = new Color(1.0f, 1.0f, 1.0f, 0.6f) * Color.green;
+				renderer.material.color *= Color.red;
 		}
 	}
 
 	/// <summary>
-	/// Places the object.
+	/// Attempts to place the object.
+	/// <returns>True if the object was placed.</returns>
 	/// </summary>
 	public bool Place()
 	{
-		if (overlapCount != 0)
+		if (CanPlace())
 		{
 			placed = true;
 			renderer.material = originalMaterial;
@@ -46,23 +69,41 @@ public class Item : MonoBehaviour {
 			return false;
 	}
 
+	/// <summary>
+	/// Tests whether the item can be placed.
+	/// </summary>
+	/// <returns>True if the item can be placed.</returns>
+	bool CanPlace()
+	{
+		foreach (GameObject g in GameObject.FindGameObjectsWithTag("Building"))
+			if (g != gameObject && Vector3.Distance(g.transform.position, transform.position) < minBuildingDistance)
+				return false;
+
+		if (overlapCount != 0)
+			return true;
+		else
+			return false;
+	}
+
+	/// <summary>
+	/// Called when the item intersects an Environment GameObject.
+	/// </summary>
+	/// <param name="other">The collider that is being intersected.</param>
 	void OnTriggerEnter(Collider other)
 	{
 		//If the item overlaps a terrain object
 		if (other.gameObject.layer == layerMask)
-		{
-			Debug.Log("Overlap: " + overlapCount);
 			overlapCount++;
-		}
 	}
 
+	/// <summary>
+	/// Called when the item no longer intersects an Environment GameObject.
+	/// </summary>
+	/// <param name="other">The collider that is no longer being intersected.</param>
 	void OnTriggerExit(Collider other)
 	{
 		//If the item no longer overlaps a terrain object
 		if (other.gameObject.layer == layerMask)
-		{
-			Debug.Log("Overlap: " + overlapCount);
 			overlapCount--;
-		}
 	}
 }
