ed279d1c23f6e8e2840f8d8a388657510731bb2c
Benjamin Cohen <bac1087@gmail.com>
2014-04-02 00:39:12 -0400

    Updated Pathfinding to Include Maze, Obstacles

---
+++ Assets/_BenCohen Test Folder/_Scripts/SeekerAI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+
+public class SeekerAI : AIPath {
+
+	public override void OnTargetReached () {
+		Debug.Log ("target reached");
+		//End of path has been reached
+		//If you want custom logic for when the AI has reached it's destination
+		//add it here
+		//You can also create a new script which inherits from this one
+		//and override the function in that script
+	}
+
+}
