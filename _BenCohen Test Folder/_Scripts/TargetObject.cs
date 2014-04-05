8a4e36ed1f78b3c8da3fb206ee6d6cb320847a4a
Benjamin Cohen <bac1087@gmail.com>
2014-04-05 15:14:52 -0400

    Re-added AStar and pathfinding changes
    Cos things dun fucked up

---
+++ _BenCohen Test Folder/_Scripts/TargetObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetObject : MonoBehaviour
+{
+		public NavMeshAgent agent;
+
+		void Start ()
+		{
+				agent.destination = transform.position;
+		}
+
+		void Update ()
+		{
+				agent.destination = transform.position;
+		}
+}
+
