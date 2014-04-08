8ac5f5f2e18dc0144e5de63eefc56225329cd4d9
Benjamin Cohen <bac1087@gmail.com>
2014-04-08 11:54:28 -0400

    Wolf now only follows within a distance

--- Assets/_BenCohen Test Folder/_Scripts/SeekerAI.cs
+++ Assets/_BenCohen Test Folder/_Scripts/SeekerAI.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using Pathfinding;
 
 
 public class SeekerAI : AIPath {
-	
+
+	/**
+	 *  Range at which the AI becomes alert and starts following
+	 **/
+	public float alertRange = 10;
+	public bool DEBUG = false;
+	private bool enabled = true;
+
 	public override void OnTargetReached () {
 		Debug.Log ("target reached");
 		//End of path has been reached
@@ -14,6 +21,24 @@ public class SeekerAI : AIPath {
 		//and override the function in that script
 	}
 
+	public override void Update () {
+		//check distance, if too far, disable, otherwise enable
+		base.Update ();
+		float dist = getDistToTarget (); 
+		if (dist <= alertRange && enabled == false) {
+			if (DEBUG) Debug.Log ("dist is " + dist + ". Path reenabled.");
+			OnEnable ();
+			enabled = true;
+		} else if (dist > alertRange && enabled == true) {
+			if (DEBUG) Debug.Log ("dist is " + dist + ". Path disabled.");
+			OnDisable ();
+			enabled = false;
+		}
 
+	}
+
+	protected float getDistToTarget () {
+		return Vector3.Distance (gameObject.transform.position, target.position);
+	}
 
 }
