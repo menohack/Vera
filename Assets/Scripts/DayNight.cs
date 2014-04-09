e5ae12eeb67078a818026b4f549fa2cc008c5a05
J D <j.dolinko@gmail.com>
2014-04-09 14:06:09 -0400

    Probably don't need DayNight.cs

---
+++ Assets/Scripts/DayNight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNight : MonoBehaviour {
+
+	/// <summary>
+	/// The timer.
+	/// </summary>
+	public float timer;
+
+	public float timeInSeconds = 0;
+
+	/// <summary>
+	/// True if it is daytime. False otherwise.
+	/// </summary>
+	private bool isDay = false;
+
+	void Update () {
+		timeInSeconds += Time.deltaTime; //just to let us know how many seconds have passed
+	}
+
+	void IsDay(bool b)
+	{
+		isDay = b;
+	}
+
+	void timeCheck(float t)
+	{
+		timer = t;
+	}
+	
+}
