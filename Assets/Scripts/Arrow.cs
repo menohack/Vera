314dae2383fb86585411adaff134571918dec7a6
James Doverspike <menohack@gmail.com>
2014-04-12 20:57:26 -0400

    Started sentry

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Arrow : MonoBehaviour {
 
 	public float SHOOT_FORCE = 10f;
 
+	DateTime birthTime;
+
+	TimeSpan liveTime = new TimeSpan(0, 0, 0, 5);
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		birthTime = DateTime.Now;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (DateTime.Now - birthTime >= liveTime)
+			Destroy(gameObject);
 	}
 
 	public void Shoot(Vector3 direction)
@@ -23,6 +30,7 @@ public class Arrow : MonoBehaviour {
 
 	void OnCollisionEnter(Collision collision)
 	{
+		Debug.Log("Arrow hit");
 		rigidbody.isKinematic = true;
 	}
 
