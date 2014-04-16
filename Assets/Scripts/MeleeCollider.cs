6f2bdefcb641dcb4239fbf84e407bba87b2bbf6e
J D <j.dolinko@gmail.com>
2014-04-16 19:37:40 -0400

    Players only hit enemies

--- Assets/Scripts/MeleeCollider.cs
+++ Assets/Scripts/MeleeCollider.cs
@@ -47,7 +47,12 @@ public class MeleeCollider : MonoBehaviour {
 		if (this.gameObject.animation.isPlaying) {
 			if (DEBUG)
 				Debug.Log (gameObject.name + " just collided with " + other.gameObject.name);
-			Health enemyHealth = other.gameObject.GetComponent<Health> ();
+
+			Health enemyHealth = null;
+			if (other.gameObject.tag == "Enemy")
+			{
+				other.gameObject.GetComponent<Health> ();
+			}
 			if (enemyHealth != null) {
 				if (DEBUG)
 					Debug.Log (gameObject.name + " just did damage to " + other.gameObject.name);
