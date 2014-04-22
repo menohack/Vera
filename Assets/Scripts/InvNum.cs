4d4bf0dc90173ec3323333fd699b4a1acd5f5abc
J D <j.dolinko@gmail.com>
2014-04-22 12:42:01 -0400

    Initial GUI done! Health, Ore, and Wood work. Dial rotation TBD.

---
+++ Assets/Scripts/InvNum.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvNum : MonoBehaviour {
+
+	/// <summary>
+	/// Wood or Ore?
+	/// </summary>
+	public bool isWood = true;
+
+	private GameObject player;
+	private Inventory inv;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		inv = player.gameObject.GetComponent<Inventory> ();
+		if (isWood) 
+		{
+			guiText.text = "" + inv.GetWood ();
+		}
+		else
+		{
+			guiText.text = "" + inv.GetOre ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isWood) 
+		{
+			guiText.text = "" + inv.GetWood ();
+		}
+		else
+		{
+			guiText.text = "" + inv.GetOre ();
+		}
+	}
+}
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -10,11 +10,11 @@ public class Inventory : MonoBehaviour {
 	int wood = 0;
 	int ore = 0;
 
-	public Texture2D oreTexture;
-	public Texture2D woodTexture;
-	public Texture2D hudTexture;
-
-	public Texture2D buttonTexture1, buttonTexture2, buttonTexture3, buttonTexture4;
+//	public Texture2D oreTexture;
+//	public Texture2D woodTexture;
+//	public Texture2D hudTexture;
+//
+//	public Texture2D buttonTexture1, buttonTexture2, buttonTexture3, buttonTexture4;
 
 	// Use this for initialization
 	void Start () {
@@ -65,48 +65,47 @@ public class Inventory : MonoBehaviour {
 		ore -= units;
 	}
 
-
-	void OnGUI()
-	{
-		//Ore and wood count
-		GUIStyle font = new GUIStyle();
-		font.fontSize = 24;
-		font.alignment = TextAnchor.MiddleCenter;
-		GUILayout.BeginArea(new Rect(0, Screen.height - 200, 200, 200));
-		GUILayout.BeginVertical();
-
-		GUILayout.BeginHorizontal();
-		GUILayout.Label(oreTexture);
-		GUILayout.Label("" + ore, font);
-		GUILayout.EndHorizontal();
-
-		GUILayout.BeginHorizontal();
-		GUILayout.Label(woodTexture);
-		GUILayout.Label("" + wood, font);
-		GUILayout.EndHorizontal();
-
-		GUILayout.EndVertical();
-		GUILayout.EndArea();
-
-		//HUD on bottom of screen with buttons
-		GUIStyle buttonStyle = new GUIStyle();
-		GUILayout.BeginArea(new Rect(Screen.width/2 - hudTexture.width/2, Screen.height - hudTexture.height, hudTexture.width, hudTexture.height), hudTexture);
-		GUILayout.BeginVertical();
-		GUILayout.FlexibleSpace();
-		GUILayout.BeginHorizontal();
-		GUILayout.FlexibleSpace();
-		GUILayout.Button(buttonTexture1, buttonStyle);
-		GUILayout.FlexibleSpace();
-		GUILayout.Button(buttonTexture2, buttonStyle);
-		GUILayout.FlexibleSpace();
-		GUILayout.Button(buttonTexture3, buttonStyle);
-		GUILayout.FlexibleSpace();
-		GUILayout.Button(buttonTexture4, buttonStyle);
-		GUILayout.FlexibleSpace();
-		GUILayout.EndHorizontal();
-		GUILayout.FlexibleSpace();
-		GUILayout.EndVertical();
-		GUILayout.EndArea();
-
-	}
+//	void OnGUI()
+//	{
+//		//Ore and wood count
+//		GUIStyle font = new GUIStyle();
+//		font.fontSize = 24;
+//		font.alignment = TextAnchor.MiddleCenter;
+//		GUILayout.BeginArea(new Rect(0, Screen.height - 200, 200, 200)); //left, top, width, height
+//		GUILayout.BeginVertical(); //stacks things on top of one another
+//
+//		GUILayout.BeginHorizontal();
+//		GUILayout.Label(oreTexture);
+//		GUILayout.Label("" + ore, font);
+//		GUILayout.EndHorizontal();
+//
+//		GUILayout.BeginHorizontal();
+//		GUILayout.Label(woodTexture);
+//		GUILayout.Label("" + wood, font);
+//		GUILayout.EndHorizontal();
+//
+//		GUILayout.EndVertical();
+//		GUILayout.EndArea();
+//
+//		//HUD on bottom of screen with buttons
+//		GUIStyle buttonStyle = new GUIStyle();
+//		GUILayout.BeginArea(new Rect(Screen.width/2 - hudTexture.width/2, Screen.height - hudTexture.height, hudTexture.width, hudTexture.height), hudTexture);
+//		GUILayout.BeginVertical();
+//		GUILayout.FlexibleSpace();
+//		GUILayout.BeginHorizontal();
+//		GUILayout.FlexibleSpace();
+//		GUILayout.Button(buttonTexture1, buttonStyle);
+//		GUILayout.FlexibleSpace();
+//		GUILayout.Button(buttonTexture2, buttonStyle);
+//		GUILayout.FlexibleSpace();
+//		GUILayout.Button(buttonTexture3, buttonStyle);
+//		GUILayout.FlexibleSpace();
+//		GUILayout.Button(buttonTexture4, buttonStyle);
+//		GUILayout.FlexibleSpace();
+//		GUILayout.EndHorizontal();
+//		GUILayout.FlexibleSpace();
+//		GUILayout.EndVertical();
+//		GUILayout.EndArea();
+//
+//	}
 }
