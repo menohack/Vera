243998c1306a531de787e4f64c0e397d5d103fa5
J D <j.dolinko@gmail.com>
2014-04-22 23:09:49 -0400

    GUI IS READY.

---
+++ Assets/Scripts/Dial.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode()] 
+public class Dial : MonoBehaviour {
+
+	public Texture2D texture;
+	public float xpos = 0;
+	public float ypos = 0;
+	private Rect rect;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void UpdateSettings () {
+		if (texture != null){
+//			GUI.DrawTexture (new Rect (30, 30, 100, 100), texture);
+			GUILayout.BeginArea(new Rect(xpos, ypos, 100, 100));
+			GUILayout.Label (texture);
+			GUILayout.EndArea();
+		}
+	}
+
+	void OnGUI() {
+		if (Application.isEditor) { UpdateSettings(); }
+	}
+}
---
+++ Assets/Scripts/RotateDialResize.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//based on GUIAspectRatioScale
+public class RotateDialResize : MonoBehaviour 
+{
+	private RotateDial myRot;
+	private Vector2 original;
+	Sundial s;
+	
+	void Start () 
+	{
+		myRot = this.gameObject.GetComponent<RotateDial>();
+		s = GameObject.FindObjectOfType<Sundial> ();
+	}
+	
+	void Update()
+	{
+		SetAngle ();
+	}
+
+	//call on an event that tells if the aspect ratio changed
+	void SetAngle()
+	{
+		//rotation
+		myRot.angle = s.GetProgress () * 360 - 90;
+	}
+}
