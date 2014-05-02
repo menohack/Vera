f43bf92a1c20ca0ec57c45e071971996efcac15c
James Doverspike <menohack@gmail.com>
2014-05-01 20:55:14 -0400

    Tweaked gate and sentry. Everything builds really well and fits together now.

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Gate : Building
+{
+	/// <summary>
+	/// The wood cost to build a gate.
+	/// </summary>
+	public static int GATE_COST_WOOD = 1;
+
+	/// <summary>
+	/// The ore cost to build a gate.
+	/// </summary>
+	public static int GATE_COST_ORE = 1;
+
+	public override int GetOreCost()
+	{
+		return GATE_COST_ORE;
+	}
+
+	public override int GetWoodCost()
+	{
+		return GATE_COST_WOOD;
+	}
+
+	protected override float HeightSpawnOffset()
+	{
+		return -0.9f * collider.bounds.extents.y;
+	}
+}
