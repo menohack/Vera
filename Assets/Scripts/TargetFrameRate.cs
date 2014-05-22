86faee3dc98cb8f2b571e8e5cd30ef417db6777b
JonathanGorr <jonathangorr@gmail.com>
2014-05-21 22:52:43 -0400

    Added script that locks fps to 30.

---
+++ Assets/Scripts/TargetFrameRate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetFrameRate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 30;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
